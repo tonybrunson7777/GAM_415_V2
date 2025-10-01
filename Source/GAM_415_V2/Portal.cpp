1b30b6b4b26fec35b7d8b529eed405cf9d4a2876
Tony Brunson <tony.brunson@snhu.edu>
2025-10-01 08:52:49 -0400

    Completed the Portal Part 1 Tutorial

---
+++ Source/GAM_415_V2/Portal.cpp
@@ -0,0 +1,113 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Portal.h"
+#include "GAM_415_V2Character.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+APortal::APortal()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// create the mesh component
+	mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+
+	// create the box component
+	boxComp = CreateDefaultSubobject<UBoxComponent>("Box Comp");
+
+	// create the capture component
+	sceneCapture = CreateDefaultSubobject<USceneCaptureComponent2D>("Capture");
+
+	// set the box component as the root component
+	RootComponent = boxComp;
+
+	// attach the mesh to the root component
+	mesh->SetupAttachment(boxComp);
+	sceneCapture->SetupAttachment(mesh);
+
+	// set the collision to ignore
+	mesh->SetCollisionResponseToAllChannels(ECR_Ignore);
+
+}
+
+// Called when the game starts or when spawned
+void APortal::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// set the render target to the scene capture component
+	boxComp->OnComponentBeginOverlap.AddDynamic(this, &APortal::OnOverlapBegin);
+
+	// set the render target to the scene capture component
+	mesh->SetHiddenInSceneCapture(true);
+
+	// check to see if mat is valid before applying it
+	if (mat)
+	{
+		mesh->SetMaterial(0, mat);
+	}
+	
+}
+
+// Called every frame
+void APortal::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	UpdatePortals();
+
+}
+
+void APortal::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AGAM_415_V2Character* playerChar = Cast<AGAM_415_V2Character>(OtherActor);
+
+	// if the player character is valid, and the other portal is valid, teleport the player to the other portal
+	if (playerChar)
+	{
+		if (OtherPortal)
+		{
+			if (!playerChar->isTeleporting)
+			{
+				playerChar->isTeleporting = true;
+				FVector loc = OtherPortal->GetActorLocation();
+				playerChar->SetActorLocation(loc);
+
+				// set a timer to reset the isTeleporting variable after 1 second
+				FTimerHandle TimerHandle;
+				FTimerDelegate TimerDelegate;
+				TimerDelegate.BindUFunction(this, "SetBool", playerChar);
+				GetWorld()->GetTimerManager().SetTimer(TimerHandle, TimerDelegate, 1, false);
+			}
+		}
+	}
+}
+
+void APortal::SetBool(AGAM_415_V2Character* playerChar)
+{
+	if (playerChar)
+	{
+		playerChar->isTeleporting = false;
+	}
+}
+
+
+void APortal::UpdatePortals()  
+{  
+	// get the difference between the two portal locations 
+   FVector Location = this->GetActorLocation() - OtherPortal->GetActorLocation();  
+
+   // get the camera location
+   FVector camLocation = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0)->GetTransformComponent()->GetComponentLocation();
+
+   // get the camera rotation
+   FRotator camRotation = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0)->GetTransformComponent()->GetComponentRotation();
+
+   // calculate the combined location 
+   FVector CombinedLocation = camLocation - Location;
+
+   // set the scene capture location and rotation
+   sceneCapture->SetWorldLocationAndRotation(CombinedLocation, camRotation);
+}
+
---
+++ Source/GAM_415_V2/Portal.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SceneCaptureComponent2D.h"
+#include "Engine/TextureRenderTarget2D.h"
+#include "Components/BoxComponent.h"
+#include "GAM_415_V2Character.h"
+#include "Portal.generated.h"
+
+
+class GAM_415_V2Character;
+
+UCLASS()
+class GAM_415_V2_API APortal : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APortal();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// variable for the mesh component
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* mesh;
+
+	// variable for the scene capture component
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USceneCaptureComponent2D* sceneCapture;
+
+	// variable for the render target
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTextureRenderTarget2D* renderTarget;
+
+	// variable for the box component
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* boxComp;
+
+	// variable for the other portal; will determine which portal in the scene that it is connected to
+	UPROPERTY(EditAnywhere)
+	APortal* OtherPortal;
+
+	// variable for the material
+	UPROPERTY(EditAnywhere)
+	UMaterialInterface* mat;
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void SetBool(AGAM_415_V2Character* playerChar);
+
+	UFUNCTION()
+	void UpdatePortals();
+
+};
